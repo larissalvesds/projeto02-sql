cb116af32bdb0c57efa2ede8dc1fa632fa02ee4a
Chayenne Pereira <hannahcho39@gmail.com>
2025-08-28 19:39:10 -0300

    Update views.sql
    Revisão das boas práticas de estruturação

--- sql/views.sql
+++ sql/views.sql
@@ -1,5 +1,11 @@
--- CRIAÇÃO DA VIEW --
--- Criando uma view para simplificar o acesso a um relatório detalhado de vendas.
+-- =============================================================================
+ --CRIAÇÃO DE VIEW
+-- =============================================================================
+
+-- Descrição:
+-- A view `v_relatorio_vendas_detalhado` é criada para simplificar o acesso
+-- a um relatório consolidado de vendas, unindo dados de clientes e produtos.
+
 CREATE OR REPLACE VIEW `t1engenhariadados.turma3_grupo7.v_relatorio_vendas_detalhado` AS
 SELECT
     V.ID_Venda,
@@ -11,9 +17,21 @@ SELECT
     V.Quantidade,
     P.Preco_Produto,
     (V.Quantidade * P.Preco_Produto) AS Valor_Total
-FROM `t1engenhariadados.turma3_grupo7.Vendas` AS V
-JOIN `t1engenhariadados.turma3_grupo7.Clientes` AS C ON V.ID_Cliente = C.ID_Cliente
-JOIN `t1engenhariadados.turma3_grupo7.Produtos` AS P ON V.ID_Produto = P.ID_Produto;
+FROM 
+    `t1engenhariadados.turma3_grupo7.Vendas` AS V
+JOIN 
+    `t1engenhariadados.turma3_grupo7.Clientes` AS C ON V.ID_Cliente = C.ID_Cliente
+JOIN 
+    `t1engenhariadados.turma3_grupo7.Produtos` AS P ON V.ID_Produto = P.ID_Produto;
 
--- Exemplo de uso da view:
-SELECT * FROM `t1engenhariadados.turma3_grupo7.v_relatorio_vendas_detalhado` WHERE Estado_Cliente = 'RJ';
+
+-- -----------------------------------------------------------------------------
+-- Exemplo de uso da VIEW
+-- -----------------------------------------------------------------------------
+
+-- Consulta para obter o relatório detalhado de vendas apenas para clientes do Rio de Janeiro (RJ).
+SELECT 
+    *
+FROM 
+    `t1engenhariadados.turma3_grupo7.v_relatorio_vendas_detalhado` 
+WHERE Estado_Cliente = 'RJ';
